f497decbec5d4d8686c4fd6ac78b80ee38518c2a
fadhil-sajeed <154217111+fadhil-sajeed@users.noreply.github.com>
2024-01-10 21:20:24 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+let slideIndex = 1;
+
+function showSlides(index) {
+  const slides = document.querySelectorAll('.slide');
+  const dots = document.querySelectorAll('.dot');
+
+  if (index > slides.length) {
+    slideIndex = 1;
+  } else if (index < 1) {
+    slideIndex = slides.length;
+  }
+
+  slides.forEach((slide) => (slide.style.display = 'none'));
+  dots.forEach((dot) => (dot.style.backgroundColor = '#bbb'));
+
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].style.backgroundColor = '#717171';
+}
+
+function currentSlide(index) {
+  showSlides((slideIndex = index));
+}
+
+showSlides(slideIndex);
+setInterval(() => showSlides(++slideIndex), 3000); // Auto slide every 3 seconds
